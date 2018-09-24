02a3e27e35076ea1e030e0ded8423d81fae72ec8
cdwyer-cognito <chris.dwyer@cognitoiq.com>
2018-09-24 21:34:36 +0100

    fixed feedback on db reload

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,6 +1,6 @@
 postMessage = async function(url){
-
-    fetch(url, {
+    let res;
+    await fetch(url, {
         method: "POST",
         cache: "no-cache",
         headers: {
@@ -8,25 +8,34 @@ postMessage = async function(url){
         },
         body: "{}"
     })
-    .then(function(message){
-        console.log(message);
-    })
+    .then(response => {res = response})
     .catch(err => console.log(err));
 
+    return res;
 }
 
-reloadLibrary = function(){
+reloadLibrary = async function(){
     console.log("Reload Library Db");
     if ( confirm("Are You Sure You Want to Load / Reload the Karaoke Library?") ) {
-        postMessage("/admin/load-library");
+        let response = await postMessage("/admin/load-library");
+
+        console.log(response);
+        
+        if ( response.status === 200 ) {
+            alert("Karaoke Library Loaded");
+        }
     }
 
 }
 
-dropRequests = function(){
+dropRequests = async function(){
     console.log("Drop Requests Db");
-    if ( confirm("Are You Sure You Want to drop the Drop the Requests Db?") ) {
-        postMessage("/admin/drop-requests");
+    if ( confirm("Are You Sure You Want to drop the Requests Db?") ) {
+        let response = await postMessage("/admin/drop-requests");
+
+        if ( response.status === 200 ) {
+            alert("Requests Database Dropped");
+        }
     }
 
 }

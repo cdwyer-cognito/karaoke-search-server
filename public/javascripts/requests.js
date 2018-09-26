0b7bb21b53fd448fad9a0cd7656bd95c8b1f7eb2
cdwyer-cognito <chris.dwyer@cognitoiq.com>
2018-09-26 16:07:24 +0100

    Added copy on click to requests page

--- public/javascripts/requests.js
+++ public/javascripts/requests.js
@@ -1,3 +1,58 @@
+(function(document) {
+	'use strict';
+
+	var LightTableFilter = (function(Arr) {
+
+		var _input;
+
+		function _onInputEvent(e) {
+			_input = e.target;
+			var tables = document.getElementsByClassName(_input.getAttribute('data-table'));
+			Arr.forEach.call(tables, function(table) {
+				Arr.forEach.call(table.tBodies, function(tbody) {
+					Arr.forEach.call(tbody.rows, _filter);
+				});
+			});
+		}
+
+		function _filter(row) {
+			var text = row.textContent.toLowerCase(), val = _input.value.toLowerCase();
+			row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
+		}
+
+		return {
+			init: function() {
+				var inputs = document.getElementsByClassName('light-table-filter');
+				Arr.forEach.call(inputs, function(input) {
+					input.oninput = _onInputEvent;
+				});
+			}
+		};
+	})(Array.prototype);
+
+	document.addEventListener('readystatechange', function() {
+		if (document.readyState === 'complete') {
+			LightTableFilter.init();
+		}
+	});
+
+})(document);
+
+// select row
+let elements= document.getElementsByTagName('td');
+for (let i=0; i<elements.length;i++) {
+    (elements)[i].addEventListener("click", function(){
+        let dummy = document.createElement("input");
+        document.body.appendChild(dummy);
+        dummy.setAttribute("id", "dummy_id");
+        document.getElementById("dummy_id").value = this.innerHTML;
+        dummy.select();
+        document.execCommand("copy");
+        document.body.removeChild(dummy);
+           
+    });
+}
+
 clickedCompleted = function(guid){
     console.log(guid);
     const jsonObj = {GUID: guid};

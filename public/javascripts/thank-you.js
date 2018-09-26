7be7bf995611d8d3166f692ab96bc47320c2bb44
cdwyer-cognito <chris.dwyer@cognitoiq.com>
2018-09-26 20:22:21 +0100

    removed log line

--- public/javascripts/thank-you.js
+++ public/javascripts/thank-you.js
@@ -2,12 +2,10 @@ onload = function(){
     let timeleft = 10;
     let downloadTimer = setInterval(function(){
         timeleft--;
-        console.log(timeleft);
-        
+    
         document.getElementById("countdown").innerHTML = "Returning to the Main Menu in: " + timeleft;
         
         if ( timeleft <= 0 ) {
-            console.log("Timer expired");
             clearInterval(downloadTimer);
             window.location.href = "/search/menu";
         }

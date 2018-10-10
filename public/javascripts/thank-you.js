4cfe2827801f5563fc5a11cde7ea822afb058093
cdwyer-cognito <chris.dwyer@cognitoiq.com>
2018-10-10 15:37:19 +0100

    changed thankyou menu

--- public/javascripts/thank-you.js
+++ public/javascripts/thank-you.js
@@ -1,16 +1,13 @@
 onload = function(){
-    let timeleft = 10;
+    let timeleft = 15;
     let downloadTimer = setInterval(function(){
         timeleft--;
-        
-        document.getElementById("countdown").innerHTML = "Returning to the Main Menu in: " + timeleft;
         
         if ( timeleft <= 0 ) {
             clearInterval(downloadTimer);
             window.location.href = "/search/menu";
-            onload();
         }
-    },1000);
+    },990);
 }
 
 navMainMenu = function(){
